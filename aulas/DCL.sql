89f56fa3c88abacf520e4716f592398a5f311b5d
DANUBIA BARBOSA CAMARGO <danubia.bcamargo@senacsp.edu.br>
2024-03-11 21:30:29 -0300

    aula 19 - revoke

---
+++ aulas/DCL.sql
@@ -0,0 +1,32 @@
+-- Listar Usuários do Sistema
+SELECT User, Host FROM mysql.user;
+
+-- Criação de um Usuário de Aplicação
+CREATE USER 'aplicacao'@'localhost'
+IDENTIFIED BY 'Aplicacao@123' ;
+
+-- Crie um usuario "Administrador" com permissao de acesso
+-- de qualquer lugar, com nossa senha padrao
+CREATE USER 'Administrador'@'%'
+IDENTIFIED BY 'Administrador@123';
+
+-- Mudar senha de usuário
+SET PASSWORD FOR 'aplicacao'@'localhost' = PASSWORD ('Aplicacao@321');
+
+-- Mudar o host do usuario
+RENAME USER 'aplicacao'@'localhost' TO 'aplicacao'@'%';
+
+-- Remover um usuario
+DROP USER 'aplicacao'@'%';
+
+-- Conceder permissoes necessarias para o usuario acessar o BD desejado
+-- GRANT tipo_de_permissao ON nome_do_banco.nome_da_tabela TO 'usuario'@'lugar'
+GRANT ALL PRIVILEGES ON gestaoacademica.* TO 'aplicacao'@'localhost';
+GRANT SELECT ON compras.pedido TO 'aplicacao'@'localhost';
+
+-- Verificar permissão por usuário
+SHOW GRANTS FOR 'aplicacao'@'localhost';
+
+-- Revogar permissões
+-- REVOKE tipo_permissao ON banco.tabela FROM 'usuario' @ 'local';
+REVOKE 
